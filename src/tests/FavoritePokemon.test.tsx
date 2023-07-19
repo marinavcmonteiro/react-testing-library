b6f85d8d632b5f36553a8f70584655531508b6cf
Marina Monteiro <marina.vcmonteiro@gmail.com>
2023-07-19 12:21:22 -0300

    Requisito 4

--- src/tests/FavoritePokemon.test.tsx
+++ src/tests/FavoritePokemon.test.tsx
@@ -1,1 +1,10 @@
-test('', () => {});
+import { render, screen } from '@testing-library/react';
+import FavoritePokemon from '../pages/About';
+
+test('Testa se é exibida na tela a mensagem No favorite pokemon found caso a pessoa não tenha Pokémon favorito.', () => {
+  render(<FavoritePokemon />);
+  const pokemons = screen.getByTestId('favorite-pokemonList');
+  if (pokemons.length === 0) {
+    expect(screen.getByText(/No favorite Pokémon found/i)).toBeInTheDocument();
+  }
+});
--- src/tests/NotFound.test.tsx
+++ src/tests/NotFound.test.tsx
@@ -1,1 +1,14 @@
-test('', () => {});
+import { render, screen } from '@testing-library/react';
+import NotFound from '../pages/NotFound';
+
+test('Testa se a página contém um heading h2 com o texto Page requested not found', () => {
+  render(<NotFound />);
+  const h2Element = screen.getByText(/Page requested not found/i);
+  expect(h2Element).toBeInTheDocument();
+});
+
+test('Testa se a página mostra a imagem https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif', () => {
+  render(<NotFound />);
+  const image = screen.getByRole('img');
+  expect(image).toHaveProperty('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+});
