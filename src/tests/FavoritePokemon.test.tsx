48e8d18d0372d788a03cdab3f3903fc0486b9471
Marina Monteiro <marina.vcmonteiro@gmail.com>
2023-07-19 19:26:03 -0300

    Refaz requisito 3

--- src/tests/FavoritePokemon.test.tsx
+++ src/tests/FavoritePokemon.test.tsx
@@ -1,10 +1,24 @@
-import { render, screen } from '@testing-library/react';
-import FavoritePokemon from '../pages/About';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../renderWithRouter';
+import FavoritePokemon from '../pages/FavoritePokemon';
+import App from '../App';
 
-test('Testa se é exibida na tela a mensagem No favorite pokemon found caso a pessoa não tenha Pokémon favorito.', () => {
-  render(<FavoritePokemon />);
-  const pokemons = screen.getByTestId('favorite-pokemonList');
-  if (pokemons.length === 0) {
-    expect(screen.getByText(/No favorite Pokémon found/i)).toBeInTheDocument();
-  }
+test('Testa se é exibida na tela a mensagem No favorite pokemon found caso a pessoa não tenha Pokémon favorito.', async () => {
+  renderWithRouter(<FavoritePokemon />);
+  expect(screen.getByText(/No favorite Pokémon found/i)).toBeInTheDocument();
+});
+
+test('Caso a pessoa não tenha Pokémon favorito, testa se são exibidos apenas os Pokémon favoritados', async () => {
+  renderWithRouter(<App />, { route: '/' });
+  const moreDetailsElement = screen.getByRole('link', { name: /more details/i });
+  await userEvent.click(moreDetailsElement);
+
+  const checkedInput = screen.getByRole('checkbox', { name: /Pokémon favoritado?/i });
+  await userEvent.click(checkedInput);
+
+  const favoriteLink = screen.getByRole('link', { name: /favorite pokémon/i });
+  await userEvent.click(favoriteLink);
+
+  expect(screen.getByText(/Pikachu/i)).toBeInTheDocument();
 });
